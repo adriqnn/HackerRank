067ca3fe37294bea5a5e15db0de5eb26b19c44b5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2022-10-12 22:21:35 +0300

    task 12

---
+++ _01_Java/_12_JavaDateAndTime.java
@@ -0,0 +1,29 @@
+package _05_HackerRank._01_Java;
+
+import java.io.*;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Calendar;
+
+public class _12_JavaDateAndTime {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+        int day = Integer.parseInt(firstMultipleInput[1]);
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = findDay(month, day, year);
+        System.out.println(res);
+
+        bufferedReader.close();
+
+    }
+    public static String findDay(int month, int day, int year) {
+        LocalDate localDate = LocalDate.of(year, month, day);
+
+        return localDate.getDayOfWeek().toString();
+    }
+}
+
