89356f75e5cdd5b77b10258407d7549612135ade
adriqnn <109970483+adriqnn@users.noreply.github.com>
2022-10-13 19:32:37 +0300

    Add files via upload

---
+++ _01_Java/_15_JavaSubstring.java
@@ -0,0 +1,15 @@
+package _05_HackerRank._01_Java;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _15_JavaSubstring {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String substring = scan.nextLine();
+        int[] numbers = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(substring.substring(numbers[0], numbers[1]));
+    }
+}
---
+++ _01_Java/zz.java
@@ -0,0 +1,21 @@
+package _05_HackerRank._01_Java;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class zz {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+
+        String line = scan.nextLine();
+
+        byte[] bytes = line.getBytes();
+
+        System.out.println();
+
+
+
+    }
+}
